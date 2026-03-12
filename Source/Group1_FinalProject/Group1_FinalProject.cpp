9e10717cb0b648abeaa6a6fa422f1ccb338e459d
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-03-12 00:15:50 -0400

    Create Unreal C++ project for final project phase 1

---
+++ Source/Group1_FinalProject/Group1_FinalProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Group1_FinalProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Group1_FinalProject, "Group1_FinalProject" );
